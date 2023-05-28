eb8a444335080337bf993fac03224e2413d15ccb
Roman Gurovich <romangurovich@gmail.com>
2023-05-27 17:49:22 -0700

    Test an OpenAI GPT4 completion

--- src/app/api/generate/route.tsx
+++ src/app/api/generate/route.tsx
@@ -1,5 +1,11 @@
 import { NextResponse } from 'next/server';
+import { OpenAI } from "langchain/llms/openai";
 
 export async function GET(request: Request) {
-    return NextResponse.json({ response: "a generated report" });
+    const model = new OpenAI({ temperature: 0, modelName: 'gpt-4' });
+    const res = await model.call(
+        "What would be a good company name a company that makes colorful socks?"
+      );
+      console.log(res);
+    return NextResponse.json({ response: res });
 }
