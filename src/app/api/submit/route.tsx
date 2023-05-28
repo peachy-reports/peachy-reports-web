7beb56c73c4bb5f8198380739270c3f83fce16c7
Roman Gurovich <romangurovich@gmail.com>
2023-05-28 11:12:53 -0700

    a real from input inflow

--- src/app/api/submit/route.tsx
+++ src/app/api/submit/route.tsx
@@ -3,27 +3,21 @@ import { OpenAI } from "langchain/llms/openai";
 import { supabase } from "../../../lib/supabaseClient";
 
 export async function POST(request: Request) {
-
     const inputs = await request.json();
-    console.log("gangly");
-    console.log(inputs)
-
+    const model = new OpenAI({ temperature: 0.75, modelName: 'gpt-4' });
     const quickModel = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' });
 
-    let { data: questions, error } = await supabase.from('questions').select('id, question_text');
+    let { data, error } = await supabase.from('sections').select('id, header, description');
     if (error) console.error(error);
-    console.log("data", questions);
 
     const promises = [];
 
-    for (let question of questions || []) {
-        const input = inputs[question.id]
-        promises.push(getAnswer(quickModel, question.question_text, input));
+    for (let section of data || []) {
+        promises.push(getSection(model, quickModel, section, inputs));
     }
 
-    const answers = await Promise.all(promises);
-    answers.push(getCo2emissions(inputs["10"]));
-    return NextResponse.json({ answers: answers });
+    const sections = await Promise.all(promises);
+    return NextResponse.json({ sections: sections });
 }
 
 async function getAnswer(model: OpenAI, question: string, input: string): Promise<string> {
@@ -38,4 +32,56 @@ function getCo2emissions(electricityUsedStr: string) {
     let electricityUsed = parseInt(electricityUsedStr)
     let co2emitted = electricityUsed * 2.21
     return `${co2emitted}t CO2`;
+}
+
+async function getSection(model: OpenAI, quickModel: OpenAI, section: any, inputs: { [key: string]: string; }): Promise<string> {
+    const { data: questionIds, error: error1 } = await supabase.from('sections_questions').select('question_id').eq('section_id', section.id);
+    if (error1) console.error(error1);
+    console.log("questionIds", questionIds);
+
+    const { data: questions, error: error2 } = await supabase.from('questions').select('id, question_text').in('id', questionIds?.map(q => q.question_id) || []);
+    if (error2) console.error(error2);
+    console.log("questions", questions);
+
+    inputs = inputs || {
+        1: "Greenie Fabrics",
+        2: "shirts, socks, and shoes",
+        3: "Las Vegas, NV",
+        4: "faith and charity",
+        5: "Biodegradable Fashion. There are a lot of child labor suppliers in the pipeline, need to avoid them. Materials extraction emits a lot of carbon into the atmosphere if not carefully sourced.",
+        6: "Emit less co2",
+        7: "Make sure providers are certified. Monitor facilities biannually",
+        8: "12000 gallons",
+        9: "propane 3k kg",
+        10: "100000",
+        11: "15 in Nevada, 3 in California",
+        12: "All employees are encouraged with a stipend to bike to work to reduce co2",
+        13: "we are the best in the west in 2022, and got the milan fashion choice award in 2021",
+        14: "John Lithgow, Emma Stone, and AZ16",
+        15: "Waste is handled by various local partners, sewing is by indiginous craftpeople"
+    };
+    const promises: Promise<string>[] = [];
+
+    questions?.forEach((question: { id: string, question_text: string}) => {
+        const id: string = question?.id;
+        const input = inputs[id];
+        promises.push(getAnswer(quickModel, question.question_text, input))
+    })
+
+    const answers = await Promise.all(promises);
+
+    const prompt = `You are a business executive with a fresh, breezy conversational but also professional tone.
+
+    Please write a single section of a sustainability report.
+    This section is called ${section.header}
+    Here's what the section is about: ${section.description}
+
+    Include the following information:
+    ${answers}
+
+    Write in the style of David Gelles
+    `;
+    const res =  await model.call(prompt);
+    await supabase.from('sections').update({ body: res }).eq('id', section.id);
+    return res;
 }
--- src/components/questionnaireForm.tsx
+++ src/components/questionnaireForm.tsx
@@ -1,7 +1,8 @@
 'use client';
-import { Input,Text, Button } from '@chakra-ui/react'
+import { Input,Text, Button, FormControl, Container, Box } from '@chakra-ui/react'
 
 import { useState } from 'react'
+
 export default function QuestionnaireForm() {
   // Handles the submit event on form submit.
   const handleSubmit = async (event: any) => {
@@ -34,15 +35,17 @@ export default function QuestionnaireForm() {
       body: JSONdata,
     };
 
+    window.open("/report/1", "_blank");
+
     // Send the form data to our forms API on Vercel and get a response.
     const response = await fetch(endpoint, options);
 
     // Get the response data from server as JSON.
     // If server returns the name submitted, that means the form works.
     const result = await response.json();
-    alert(`Is this your full data: ${result.answers}`);
   };
 	return (
+    <Container>
 	  <form onSubmit={handleSubmit}>
     <Example name="1" question={"What is the name of your company?"}></Example>
     <Example name="2" question={"What does your company make?"}></Example>
@@ -50,8 +53,10 @@ export default function QuestionnaireForm() {
 		<Example name="4" question={"What are the core values of your company?"}></Example>
     <Example name="5" question={"What sector are you in? What are the sustainability risks and opportunities for the industry?"}></Example>
 		<Example name="6" question={"What are your main sustainability goals for the next year? Can you provide 3 metrics to quantify this?"}></Example>
-		<Example name="7" question={"Are you undertaking any ESG initiatives, certifications, grants, etc. in support of these goals? If not, and you'd like us to help you brainstorm, click this button."}></Example>
-		<Button colorScheme='blue'>Button</Button>
+		<FormControl>
+      <Example name="7" question={"Are you undertaking any ESG initiatives, certifications, grants, etc. in support of these goals? If not, and you'd like us to help you brainstorm, click this button."}></Example>
+		  <Button colorScheme='blue'>Not sure? Let us generate some for you!</Button>
+    </FormControl>
 		<Example name="8" question={"Now let's figure out your rough emissions. If you use vehicles as part of your operation, how much fuel did you use last year? If you didn't use vehicles, please put 0."}></Example>
 		<Example name="9" question={"If you use fossil fuels to heat or cool your facilities, or to generate power on site, what type of fuel did you burn? How much did you burn?"}></Example>
     <Example name="10" question={"How much electricity did you use across all company locations (in kwh)?"}></Example>
@@ -62,6 +67,7 @@ export default function QuestionnaireForm() {
     <Example name="15" question={"Do you have any data on your materials consumption, suppliers (locations, energy usage), waste or transportation you would like to share? If so please describe below."}></Example>
     <Button type="submit">Submit</Button>
     </form>
+    </Container>
 		)
 	;
 }
@@ -76,7 +82,7 @@ function Example({ name, question }: questionProps) {
   const handleChange = (event: any) => setValue(event.target.value);
 
   return (
-    <>
+    <Box pt={4} pb={4}>
       <Text>{question}</Text>
       <Input
         name={name}
@@ -85,6 +91,6 @@ function Example({ name, question }: questionProps) {
         placeholder={'  '}
         size='md'
       />
-    </>
+    </Box>
   );
 }
