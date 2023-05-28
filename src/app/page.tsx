3f36a2cb79f27e6e0c4a31684601d4b818927d47
brianreed21 <brian.reed21@gmail.com>
2023-05-28 10:47:00 -0700

    updated landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
+'use client';
+
+import { Image, Text, Button, Link } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 export default function Home() {
   return (
-    <main>
-        <h1>SusReport</h1>
-        <h2>Make a 2-pager sustainability report in 10 minutes</h2>
-    </main>
+    <div>
+    	<Image
+		  borderRadius='full'
+		  boxSize='150px'
+		  src='https://i.im.ge/2023/05/28/hiqTgh.noun-peach-1554586.png'
+		  alt='Dan Abramov'
+		/>
+    	<Text fontSize={36}>Peachy</Text>
+        <h1></h1>
+        <h2></h2>
+        <Text fontSize={24}>We make sustainability reporting quick, easy, and accessible. </Text>
+        <h3> </h3>
+        <h4>We'll generate your sustainability report in 15 minutes.  </h4>
+        <h4>No prior sustainability expertise required.  </h4>
+        <h4>Fill in this  form and we will do the rest.  </h4>
+        <h5> </h5>
+        <Button colorScheme='teal' size='lg'>
+		  <Link color='white' href='http://localhost:3000/start'>
+		    Get started.
+		  </Link> </Button>
+		  <h5> </h5>
+		  <h5> </h5>
+    </div>
   )
 }
