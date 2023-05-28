8cd896a58f365fde26800cb29c7526dcc87def9b
Rick G <lacunainc04@gmail.com>
2023-05-27 21:59:14 -0700

    fix deploy issues

---
+++ src/app/report/[id]/components/main.tsx
@@ -0,0 +1,36 @@
+"use client"
+import React, {useEffect, useState} from "react";
+import { useParams } from 'next/navigation';
+
+async function Main() {
+    const [data, setData] = useState<any>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        fetch('/api/report/generate/123', { cache: 'force-cache' })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("once!");
+                setError(data.error)
+                setData(data.response)
+                setLoading(false)
+            })
+    }, []);
+
+    if (loading) return (
+        <div>Starting Generation</div>
+    )
+
+    return (
+        <div>
+            {error && <div>Something bad happened</div>}
+            {data && <div>
+                {data?.meta?.name}
+            </div>}
+            <p>Sure!!!</p>
+        </div>
+    );
+}
+
+export default Main;
--- src/app/report/[id]/page.tsx
+++ src/app/report/[id]/page.tsx
@@ -1,42 +1,11 @@
 "use client"
-import { useParams } from 'next/navigation';
-import React, {useEffect, useState} from "react";
-
-async function Quote() {
-    const [data, setData] = useState<any>(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState("");
-
-    useEffect(() => {
-        fetch('/api/report/generate/123', { cache: 'force-cache' })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log("once!");
-                setError(data.error)
-                setData(data.response)
-                setLoading(false)
-            })
-    }, []);
-    
-    if (loading) return (
-        <div>Starting Generation</div>
-    )
-
-    return (
-        <div>
-            {error && <div>Something bad happened</div>}
-            {data && <div>
-                {data?.meta?.name}
-            </div>}
-            <p>Sure!!!</p>
-        </div>
-    );
-}
+import {Main} from "next/document";
+import React from 'react';
 
 export default async function Page() {
     return <main>
         <React.Suspense fallback={<p>Fast component loading...</p>}>
-            <Quote />
+            <Main />
         </React.Suspense>
     </main>;
 }
