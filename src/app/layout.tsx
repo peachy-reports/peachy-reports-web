f47a209e45a199930da7b943d235ef99a06f2768
Rick G <lacunainc04@gmail.com>
2023-05-28 01:32:47 -0700

    Init first styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 'use client'
-import { Inter } from 'next/font/google'
 import ThemeProvider from './theme-provider';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -11,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
--- src/app/theme-provider.tsx
+++ src/app/theme-provider.tsx
@@ -1,14 +1,17 @@
 'use client';
+import '@fontsource/source-sans-pro/600.css';
+import '@fontsource/source-sans-pro/900.css';
 
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
+import theme from "@/components/themes";
 
 export default function ThemeProvider({ children }: { 
     children: React.ReactNode 
     }) {
   return (
     <CacheProvider>
-         <ChakraProvider>
+         <ChakraProvider theme={theme}>
       {children}
     </ChakraProvider >
     </CacheProvider>
