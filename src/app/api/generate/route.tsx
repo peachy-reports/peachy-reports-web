73b76f600333ce365d79fa9adcd1a4d05e52185e
Roman Gurovich <romangurovich@gmail.com>
2023-05-27 20:54:56 -0700

    Use GPT for forming answers and add Supabase client

--- src/app/api/generate/route.tsx
+++ src/app/api/generate/route.tsx
@@ -1,11 +1,29 @@
 import { NextResponse } from 'next/server';
 import { OpenAI } from "langchain/llms/openai";
+import { supabase } from "../../../lib/supabaseClient";
 
 export async function GET(request: Request) {
     const model = new OpenAI({ temperature: 0, modelName: 'gpt-4' });
-    const res = await model.call(
-        "What would be a good company name a company that makes colorful socks?"
-      );
-      console.log(res);
-    return NextResponse.json({ response: res });
+    const simpleModel = new OpenAI({ temperature: 0, modelName: 'gpt-4' });
+
+    const questions = ["What does your company make?", "How old is your company?"];
+    const inputs = ["shirts, socks, and shoes", "3 years old"];
+    const promises = [];
+
+    for (const i of Array.from(questions.keys())) {
+        promises.push(getAnswer(model, questions[i], inputs[i]))
+        console.log(i);
+      }
+
+    const answers = await Promise.all(promises);
+
+    return NextResponse.json({ response: answers });
+}
+
+async function getAnswer(model: OpenAI, question: string, input: string): Promise<string> {
+    const prompt = `Reword the following question, answer pair as a statement:
+    Question: ${question}
+    Answer: ${input}`;
+
+    return await model.call(prompt);
 }
